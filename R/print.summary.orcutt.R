43f4c90a6bf6815191611bb503ba4bca25ffb260
Alfredo Romero <aaromero@ncat.edu>
2025-03-23 12:35:49 -0400

    Version 0.0.1

--- R/print.summary.orcutt.R
+++ R/print.summary.orcutt.R
@@ -1,19 +1,34 @@
-print.summary.orcutt <-
-function(x, ...)
-{
-  cat("Call:\n")
-  print(x$call)
-  cat("\n")
-  printCoefmat(x$coefficients, P.values=TRUE, has.Pvalue=TRUE, signif.stars = getOption("show.signif.stars"),
-               signif.legend = TRUE)
-  
-  cat("\nResidual standard error:", x$stat[1],"on", x$stat[2] ,"degrees of freedom")
-  cat("\nMultiple R-squared: ", x$stat[3], ",  Adjusted R-squared: ",  x$stat[4])
-  cat("\nF-statistic:", x$stat[5] , "on", x$stat[6] ,"and", x$stat[2] ,"DF,  p-value: <", 
-      format(x$stat[7], scientific=TRUE, digits = 4))
-  
-  #Durbin-Watson
-  cat("\n\nDurbin-Watson statistic \n(original):   ", format(round(x$DW.t[1],5), nsmall=5),", p-value:", format(x$DW.t[2], scientific=TRUE, digits = 4))
-  cat("\n(transformed):", format(round(x$DW.t[3],5), nsmall=5),", p-value:", format(x$DW.t[4], scientific=TRUE, digits = 4))
-  
+#' Print method for summary of Cochrane-Orcutt model
+#'
+#' Prints a formatted summary of a Cochrane-Orcutt model, including coefficients,
+#' residual statistics, R-squared, F-statistic, and Durbin-Watson diagnostics.
+#'
+#' @param x An object of class \code{summary.orcutt}.
+#' @param ... Additional arguments (currently unused).
+#' @export
+print.summary.orcutt <- function(x, ...) {
+    cat("Call:\n")
+    print(x$call)
+    cat("\n")
+
+    printCoefmat(
+        x$coefficients,
+        P.values = TRUE,
+        has.Pvalue = TRUE,
+        signif.stars = getOption("show.signif.stars"),
+        signif.legend = TRUE
+    )
+
+    cat("\nResidual standard error:", x$stat[1], "on", x$stat[2], "degrees of freedom")
+    cat("\nMultiple R-squared: ", x$stat[3], ",  Adjusted R-squared: ", x$stat[4])
+    cat("\nF-statistic:", x$stat[5], "on", x$stat[6], "and", x$stat[2], "DF,  p-value: <",
+        format(x$stat[7], scientific = TRUE, digits = 4))
+
+    # Durbin-Watson
+    cat("\n\nDurbin-Watson statistic \n(original):   ",
+        format(round(x$DW.t[1], 5), nsmall = 5),
+        ", p-value:", format(x$DW.t[2], scientific = TRUE, digits = 4))
+    cat("\n(transformed):",
+        format(round(x$DW.t[3], 5), nsmall = 5),
+        ", p-value:", format(x$DW.t[4], scientific = TRUE, digits = 4))
 }
--- R/summary.orcutt.R
+++ R/summary.orcutt.R
@@ -1,34 +1,44 @@
-summary.orcutt <-
-function(object, ...)
-{
-  TAB <- cbind(Estimate = coef(object),
-               StdErr = object$std.error,
-               t.value = round(object$t.value,3),
-               p.value = object$p.value)
-  colnames(TAB) <- c("Estimate", "Std. Error" ,"t value" ,"Pr(>|t|)")
-  
-  stat <- c(round(object$rse,4),
-            object$df.residual,
-            round(object$r.squared,4),
-            round(object$adj.r.squared,4),
-            round(object$Fs[1],1),
-            object$rank,
-            object$Fs[2])
-  
-  r.squared <- round(object$r.squared,4)
-  adj.r.squared <- round(object$adj.r.squared,4)
-  
-  fstatistic <- c(round(object$Fs[1],1), object$stat[7])
-  
-  df <- c(object$rank, object$df.residual)
-  
-  DW.t <- object$DW
-  
-  res <- list(call=object$call,
-              coefficients=TAB, 
-              stat = stat,
-              DW.t = DW.t)
-  
-  class(res) <- "summary.orcutt"
-  res
+#' Summary method for Cochrane-Orcutt estimation
+#'
+#' Constructs a summary object for a fitted Cochrane-Orcutt model, including coefficient estimates,
+#' standard errors, t-values, p-values, and model statistics.
+#'
+#' @param object An object of class \code{orcutt}.
+#' @param ... Additional arguments (currently unused).
+#' @return An object of class \code{summary.orcutt}, containing:
+#' \describe{
+#'   \item{call}{The model call.}
+#'   \item{coefficients}{A matrix with estimates, standard errors, t-values, and p-values.}
+#'   \item{stat}{A vector of model summary statistics.}
+#'   \item{DW.t}{Durbin-Watson statistics.}
+#' }
+#' @export
+summary.orcutt <- function(object, ...) {
+    TAB <- cbind(
+        Estimate = coef(object),
+        StdErr = object$std.error,
+        t.value = round(object$t.value, 3),
+        p.value = object$p.value
+    )
+    colnames(TAB) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+
+    stat <- c(
+        round(object$rse, 4),
+        object$df.residual,
+        round(object$r.squared, 4),
+        round(object$adj.r.squared, 4),
+        round(object$Fs[1], 1),
+        object$rank,
+        object$Fs[2]
+    )
+
+    res <- list(
+        call = object$call,
+        coefficients = TAB,
+        stat = stat,
+        DW.t = object$DW
+    )
+
+    class(res) <- "summary.orcutt"
+    res
 }
