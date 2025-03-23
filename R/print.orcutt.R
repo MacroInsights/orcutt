744c7fec89303526d05af7190d6178202a8bfa84
Alfredo Romero <aaromero@ncat.edu>
2025-03-23 10:23:27 -0400

    Updating print.orcutt.R

--- R/print.orcutt.R
+++ R/print.orcutt.R
@@ -1,27 +1,33 @@
-print.orcutt <-
-function (x, ...){
-  # title
-  cat("Cochrane-orcutt estimation for first order autocorrelation \n \n")
+#' Print method for Cochrane-Orcutt estimation
+#'
+#' Displays results from a Cochrane-Orcutt model fit.
+#'
+#' @param x An object of class \code{orcutt}.
+#' @param ... Additional arguments (currently unused).
+#' @export
+print.orcutt <- function(x, ...) {
+    # title
+    cat("Cochrane-orcutt estimation for first order autocorrelation \n \n")
 
+    # formula
+    cat("Call:\n")
+    print(x$call)
 
-  # formula
-  cat("Call:\n")
-  print(x$call)
+    # number of iterations
+    cat("\n number of interactions:" , x$number.interaction)
 
-  # number of interaction
-  cat("\n number of interactions:" , x$number.interaction)
+    # rho
+    cat("\n rho" , round(x$rho, 6))
 
-  # rho
-  cat("\n rho" , round(x$rho,6))
+    # Durbin-Watson
+    cat("\n\nDurbin-Watson statistic \n(original):   ",
+        format(round(x$DW[1], 5), nsmall = 5),
+        ", p-value:", format(x$DW[2], scientific = TRUE, digits = 4))
+    cat("\n(transformed):",
+        format(round(x$DW[3], 5), nsmall = 5),
+        ", p-value:", format(x$DW[4], scientific = TRUE, digits = 4))
 
-
-  #Durbin-Watson
-  cat("\n\nDurbin-Watson statistic \n(original):   ", format(round(x$DW[1],5), nsmall=5),", p-value:", format(x$DW[2], scientific=TRUE, digits = 4))
-  cat("\n(transformed):", format(round(x$DW[3],5), nsmall=5),", p-value:", format(x$DW[4], scientific=TRUE, digits = 4))
-
-
-  # coefficients
-  cat("\n \n coefficients: \n" )
-  print(round(x$coefficients,6))
-
+    # coefficients
+    cat("\n \n coefficients: \n")
+    print(round(x$coefficients, 6))
 }
